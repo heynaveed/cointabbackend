6d35a0131faad9add121243bcf0ee7bc7f892d1e
heynaveed <naveedpasha0058@gmail.com>
2023-02-26 16:09:12 +0530

    Updated UserDetails

--- Routes/UserDetailsRoute.js
+++ Routes/UserDetailsRoute.js
@@ -4,11 +4,12 @@ const { UserModel } = require("../Model/UserModel");
 const UserDetails = Router();
 
 UserDetails.get("/", async (req, res) => {
-    const { page, limit, gender } = req.body;
-    let ress = gender ? await UserModel.find({ gender: gender }).skip((page - 1) * limit).limit(limit) : await UserModel.find({}).skip((page - 1) * limit).limit(limit)
-    console.log(ress)
-    console.log(ress.length);
-    res.send(ress);
+    const {page,limit,gender } = req.body;
+    console.log(page,limit,gender)
+    let users = gender ? await UserModel.find({ gender: gender }).skip((page - 1) * limit).limit(limit) : await UserModel.find({}).skip((page - 1) * limit).limit(limit)
+    console.log(users)
+    console.log(users.length);
+    res.send(users);
 })
 
 module.exports = { UserDetails }
